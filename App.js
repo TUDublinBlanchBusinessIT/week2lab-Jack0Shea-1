16d6f6170a571d90ceafb0c6a46d340dfcd5bd2f
Jack0Shea <158283503+Jack0Shea@users.noreply.github.com>
2024-09-23 16:46:30 +0100

    CSS

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import {View, Text} from 'react-native' ;
+import {View, Text, StyleSheet} from 'react-native' ;
+
+const styles = StyleSheet.create({
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
+
 
 export default function App(){
+
+
   return(
-    <view>
-      <text>Hello, World</Text>
-    </view>
+    <View>
+      <Text style={styles.paragraph}>Hello, World</Text>
+    </View>
   )
 };
